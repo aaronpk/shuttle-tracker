b36b76b12e584b68e4dea86dd6e782fb40fabb0f
Aaron Parecki <aaron@parecki.com>
2016-09-05 16:41:43 +0000

    store geojson for me.php

--- me.php
+++ me.php
@@ -4,10 +4,15 @@ include('lib/inc.php');
 header('Content-type: application/json');
 
 $data = array(
-  'id' => $_GET['uniq'],
-  'date' => date('Y-m-d H:i:s'),
-  'lat' => $_GET['lat'],
-  'lng' => $_GET['lng'],
+	'type' => 'Feature',
+	'geometry' => array(
+		'type' => 'Point',
+		'coordinates' => array((double)$_GET['lng'], (double)$_GET['lat'])
+	),
+	'properties' => array(
+    'id' => $_GET['uniq'],
+    'date' => date('Y-m-d H:i:s')
+  )
 );
 
 $redis->lpush('xoxo-visitors', json_encode($data));
