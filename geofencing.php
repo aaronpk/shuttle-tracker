b59b360c6f261bd6943f4c1b9e50f7c368cf8d7a
Aaron Parecki <aaron@parecki.com>
2016-09-05 16:14:11 +0000

    also geofence bridges

--- geofencing.php
+++ geofencing.php
@@ -1,16 +1,29 @@
 <?php
 include('lib/inc.php');
 
+echo "Starting...\n";
+
 $stops = json_decode(file_get_contents('stops.geojson'));
+$bridges = [
+  ['name'=>'burnside','coordinates'=>[-122.66786813735963,45.522871441089144]],
+  ['name'=>'morrison','coordinates'=>[-122.66934871673583,45.51801532361445]]
+];
+
 
 foreach($stops as $stop) {
   if(get_stop_state($redis, $stop->properties->Name) === false) {
-    echo "Setting default state\n";
+    echo "Setting default state for ".$stop->properties->Name."\n";
     set_stop_state($redis, $stop->properties->Name, 'outside');
   }
 }
+foreach($bridges as $bridge) {
+  if(get_stop_state($redis, $bridge['name']) === false) {
+    echo "Setting default state for ".$bridge['name']."\n";
+    set_stop_state($redis, $bridge['name'], 'outside');
+  }
+}
 
-$redis->subscribe(['xoxo-tracker'], function($r, $channel, $data) use($stops) {
+$redis->subscribe(['xoxo-tracker'], function($r, $channel, $data) use($stops,$bridges) {
   $redis2 = new Redis();
   $redis2->connect('127.0.0.1', 6379);
 
@@ -19,7 +32,7 @@ $redis->subscribe(['xoxo-tracker'], function($r, $channel, $data) use($stops) {
 
   foreach($stops as $stop) {
     if(geo\gcDistance($stop->geometry->coordinates[1], $stop->geometry->coordinates[0],
-      $data->geometry->coordinates[1], $data->geometry->coordinates[0]) <= 20) {
+      $data->geometry->coordinates[1], $data->geometry->coordinates[0]) <= 40) {
       // Shuttle is inside this stop right now
       // If the shuttle was previously outside this stop, trigger a notification
       if(($state=get_stop_state($redis2, $stop->properties->Name)) == 'outside') {
@@ -39,7 +52,24 @@ $redis->subscribe(['xoxo-tracker'], function($r, $channel, $data) use($stops) {
         set_stop_state($redis2, $stop->properties->Name, 'outside');
     }
   }
+  
+  foreach($bridges as $bridge) {
+    if(geo\gcDistance($bridge['coordinates'][1], $bridge['coordinates'][0],
+      $data->geometry->coordinates[1], $data->geometry->coordinates[0]) <= 200) {
+      // Shuttle is on this bridge
+      if(($state=get_stop_state($redis2, $bridge['name'])) == 'outside') {
+        post_to_slack('The shuttle is crossing the ' . ucfirst($bridge['name']) . ' bridge');
+      }
+      if($state != 'inside')
+        set_stop_state($redis2, $bridge['name'], 'inside');
+    } else {
+      $state = get_stop_state($redis2, $bridge['name']);
+      if($state != 'outside')
+        set_stop_state($redis2, $bridge['name'], 'outside');
+    }
+  }
 
+  $redis2->close();
 });
 
 function get_stop_state(&$r, $stop) {
@@ -63,5 +93,5 @@ function post_to_slack($msg) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_POST, true);
   curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('payload'=>json_encode($payload))));
-  #$response = curl_exec($ch);  
+  $response = curl_exec($ch);  
 }
